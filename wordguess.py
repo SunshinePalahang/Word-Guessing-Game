480ebb870d5626e397be3d513ecaa1acfdff2992
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2022-11-19 10:24:37 +0800

    Implemented random module

--- wordguess.py
+++ wordguess.py
@@ -4,6 +4,12 @@
 # The user will only have 30 chances. (not limited characters each chances)
 
 # randomly pick 10 words
+from random import choice
+
+with open ('words.txt') as file:
+    all_words = [line.strip().lower() for line in file if not line.startswith('#')]
+
+words = [choice(all_words) for n in range (10)]
 # user input
-# user correct guess
+# user correct guess and chances left
 # user score
