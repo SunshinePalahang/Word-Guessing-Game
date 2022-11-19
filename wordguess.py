8818284fb0634ed6d16247f64140a34642a36896
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2022-11-19 10:17:09 +0800

    program guide

---
+++ wordguess.py
@@ -0,0 +1,9 @@
+# Word Guessing Game
+# Generate 10 random words from a list of words and then the user will
+# have to guess those words by guessing its characters/letters using input.
+# The user will only have 30 chances. (not limited characters each chances)
+
+# randomly pick 10 words
+# user input
+# user correct guess
+# user score
